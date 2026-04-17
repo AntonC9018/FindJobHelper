7169aea8b3990c359ec56542a9ba2a534abe577e
AntonC9018 <anton.curm@gmail.com>
2026-04-17 14:57:26 +0300

    Rich text

--- src/MainCli/RichText.cs
+++ src/MainCli/RichText.cs
@@ -1,5 +1,10 @@
+using System.Collections;
+using System.Collections.Frozen;
 using System.Collections.Immutable;
-
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 using static RichTextFactory;
 
 public sealed class RichTextFactory
@@ -28,7 +33,13 @@ public sealed class RichTextFactory
         return Styled(text, StyleFlags.Code);
     }
 
-    public static Href Href(string url, RichText text)
+    public static Href Href(string url, RichTextInterpolatedStringHandler rt)
+    {
+        var ret = Href(url, rt.Build());
+        return ret;
+    }
+
+    public static Href Href(string url, IRichTextNode text)
     {
         if (!Uri.TryCreate(url, UriKind.Absolute, out var uri))
         {
@@ -42,7 +53,7 @@ public sealed class RichTextFactory
     }
 }
 
-public interface IRichTextElement
+public interface IRichTextNode
 {
 }
 
@@ -54,7 +65,7 @@ public enum StyleFlags
     Code = 1 << 2,
 }
 
-public sealed class StyledText : IRichTextElement
+public sealed class StyledText : IRichTextNode
 {
     public required string Text { get; init; }
     public required StyleFlags Style { get; init; }
@@ -62,22 +73,27 @@ public sealed class StyledText : IRichTextElement
     public override string ToString() => Text;
 }
 
-public sealed class Href : IRichTextElement
+public sealed class Href : IRichTextNode
 {
     public required Uri Url { get; init; }
-    public required IRichTextElement Text { get; init; }
+    public required IRichTextNode Text { get; init; }
 
     public override string ToString() => Text.ToString()!;
 }
 
-public sealed class RichText : IRichTextElement
+public sealed class RichText : IRichTextNode
 {
-    public required ImmutableArray<IRichTextElement> Items { get; init; }
+    public required ImmutableArray<IRichTextNode> Items { get; init; }
 
-    public override string ToString() => string.Join(" ", Items);
+    public override string ToString() => string.Join("", Items);
+
+    public static RichText Create(RichTextInterpolatedStringHandler rt)
+    {
+        return rt.Build();
+    }
 }
 
-public sealed class PlainText : IRichTextElement
+public sealed class PlainText : IRichTextNode
 {
     public required string Text { get; init; }
 
@@ -86,18 +102,473 @@ public sealed class PlainText : IRichTextElement
 
 public static class ExampleUsage
 {
-    // Use an interpolated string handler here.
-    private static RichText T(RichText rt)
-    {
-        return rt;
-    }
+
 
     public static void Test()
     {
-        _ = T($"""
+        _ = RichText.Create($"""
         Hello this is plain text,
-        Here's a link: {Href("https://example.com", $"{Italic("Hello")} world"))}.
+        Here's a link: {Href("https://example.com", $"{Italic("Hello")} world")}.
         Here's some text: {Styled("", StyleFlags.Bold | StyleFlags.Italic)} {Bold("bold")} {Code("code")}
         """);
     }
+}
+
+public readonly struct RichTextBuilder(int len)
+{
+    private readonly ImmutableArray<IRichTextNode>.Builder Builder =
+        ImmutableArray.CreateBuilder<IRichTextNode>(len);
+
+    public void Add(IRichTextNode t)
+    {
+        Builder.Add(t);
+    }
+
+    public RichText Build()
+    {
+        var x = Builder.DrainToImmutable();
+        return new()
+        {
+            Items = x,
+        };
+    }
+}
+
+[InterpolatedStringHandler]
+public readonly struct RichTextInterpolatedStringHandler
+{
+    private readonly RichTextBuilder _builder;
+
+    public RichTextInterpolatedStringHandler(
+        int literalLen,
+        int formattedCount)
+    {
+        int segmentCountEstimate = formattedCount + literalLen / 8;
+        _builder = new(segmentCountEstimate);
+    }
+
+    public void AppendLiteral(string s)
+    {
+        if (s.Length == 0)
+        {
+            return;
+        }
+        _builder.Add(new PlainText
+        {
+            Text = s,
+        });
+    }
+
+    public void AppendFormatted(IRichTextNode rt)
+    {
+        _builder.Add(rt);
+    }
+
+    public RichText Build()
+    {
+        return _builder.Build();
+    }
+}
+
+public delegate void DefaultVisit(IRichTextNode node, RichTextVisitor visitor);
+public delegate DefaultVisit Visit(DefaultVisit next);
+public delegate void DefaultVisit<T>(T node, RichTextVisitor visitor);
+public delegate DefaultVisit<T> Visit<T>(DefaultVisit<T> next);
+public delegate IEnumerator<IRichTextNode> GetChildren(IRichTextNode node);
+public delegate IEnumerator<IRichTextNode> GetChildren<T>(T node);
+
+public sealed class RichTextVisitationMapBuilder
+{
+    private sealed class Impl()
+    {
+        public Delegate? Default = null;
+        public Delegate? GetChildren = null;
+        public List<Delegate> List = new(1);
+    }
+
+    private DefaultVisit? _unhandled = null;
+    private Visit? _each = null;
+    private readonly Dictionary<Type, Impl> _impls;
+
+    public RichTextVisitationMapBuilder(RichTextVisitationMapBuilder other)
+    {
+        _impls = other._impls.ToDictionary();
+    }
+
+    public RichTextVisitationMapBuilder()
+    {
+        _impls = new();
+    }
+
+    // private Visit Wrap<T>(Visit<T> func) => (node, visitor) => func((T) node, visitor);
+
+    private Impl GetOrAdd<T>()
+    {
+        ref var x = ref CollectionsMarshal.GetValueRefOrAddDefault(_impls, typeof(T), out bool exists);
+        if (!exists)
+        {
+            x = new();
+        }
+        return x!;
+    }
+
+    public RichTextVisitationMapBuilder Children<T>(GetChildren<T> func)
+    {
+        var x = GetOrAdd<T>();
+        x.GetChildren = func;
+        return this;
+    }
+
+    public RichTextVisitationMapBuilder OnUnhandled(DefaultVisit func)
+    {
+        _unhandled = func;
+        return this;
+    }
+
+    public RichTextVisitationMapBuilder SetDefault<T>(DefaultVisit<T> func)
+    {
+        var x = GetOrAdd<T>();
+        x.Default = func;
+        return this;
+    }
+
+    public RichTextVisitationMapBuilder DefaultDoNothing<T>()
+    {
+        return SetDefault<T>((a, b) =>
+        {
+            _ = a;
+            _ = b;
+        });
+    }
+
+    public RichTextVisitationMapBuilder Override<T>(Visit<T> func)
+        where T : IRichTextNode
+    {
+        var x = GetOrAdd<T>();
+        x.List.Add(func);
+        return this;
+    }
+
+    public RichTextVisitationMapBuilder Each(Visit func)
+    {
+        _each = func;
+        return this;
+    }
+
+    public RichTextVisitationMapBuilder Copy() => new(this);
+
+    public RichTextVisitationMap Build()
+    {
+        var unhandled = _unhandled ?? (static (a, b) =>
+        {
+            _ = b;
+            throw new InvalidOperationException($"Unhandled type {a.GetType()}");
+        });
+        return new(unhandled, Ret().ToFrozenDictionary());
+
+        IEnumerable<KeyValuePair<Type, RichTextVisitationMap.Impl>> Ret()
+        {
+            foreach (var (type, list) in _impls)
+            {
+                var factory = GenericHandleList
+                    .MakeGenericMethod(type)
+                    .CreateDelegate<Func<Impl, RichTextVisitationMap.Impl>>(this);
+                var handler = factory(list);
+                // Do it like this for now.
+                if (_each is { } each)
+                {
+                    var prev = handler.Visit;
+                    handler = handler with
+                    {
+                        Visit = each(prev),
+                    };
+                }
+                yield return new(type, handler);
+            }
+        }
+    }
+
+    private RichTextVisitationMap.Impl HandleList<T>(Impl list)
+        where T : IRichTextNode
+    {
+        var span = CollectionsMarshal.AsSpan(list.List);
+        var current = ((DefaultVisit<T>?) list.Default);
+
+        GetChildren? getChildrenRet = null;
+        {
+            if (list.GetChildren is { } getChildren)
+            {
+                var c = (GetChildren<T>) getChildren;
+                getChildrenRet = n => c((T) n);
+            }
+        }
+
+        {
+            if (current is null && list.GetChildren is { } getChildren)
+            {
+                var c = (GetChildren<T>) getChildren;
+                current = (a, b) =>
+                {
+                    using var children = c(a);
+                    while (true)
+                    {
+                        if (!children.MoveNext())
+                        {
+                            return;
+                        }
+                        if (b.Data.Action != VisitationAction.Recurse)
+                        {
+                            return;
+                        }
+                        b.Visit(children.Current);
+                    }
+                };
+            }
+        }
+        if (current is null)
+        {
+            current = static (a, b) =>
+            {
+                _ = a;
+                _ = b;
+            };
+        }
+        foreach (var x in span)
+        {
+            var t = (Visit<T>) x;
+            current = t(current);
+        }
+        return new(
+            Visit: (a, b) => current((T) a, b),
+            GetChildren: getChildrenRet);
+    }
+
+    private static readonly MethodInfo GenericHandleList = typeof(RichTextVisitationMapBuilder)
+        .GetMethod(nameof(HandleList), BindingFlags.Instance | BindingFlags.NonPublic)!;
+}
+
+
+
+public sealed class RichTextVisitationMap
+{
+    internal readonly record struct Impl(DefaultVisit Visit, GetChildren? GetChildren);
+    private readonly DefaultVisit _onUnhandled;
+    private readonly FrozenDictionary<Type, Impl> _impls;
+    private static readonly CompletedEnumeratorT CompletedEnumerator = new();
+
+    private sealed class CompletedEnumeratorT : IEnumerator<IRichTextNode>
+    {
+        public bool MoveNext() => false;
+        public void Reset()
+        {
+        }
+        public IRichTextNode Current => throw new InvalidOperationException();
+        object? IEnumerator.Current => Current;
+        public void Dispose()
+        {
+        }
+    }
+
+    internal RichTextVisitationMap(
+        DefaultVisit onUnhandled,
+        FrozenDictionary<Type, Impl> impls)
+    {
+        _impls = impls;
+        _onUnhandled = onUnhandled;
+    }
+
+    public DefaultVisit GetHandler(Type type)
+    {
+        if (_impls.TryGetValue(type, out var i))
+        {
+            return i.Visit;
+        }
+        return _onUnhandled;
+    }
+
+    public IEnumerator<IRichTextNode> GetChildren(IRichTextNode node)
+    {
+        var type = node.GetType();
+        if (_impls.TryGetValue(type, out var t) && t.GetChildren is { } getChildren)
+        {
+            return getChildren(node);
+        }
+        return CompletedEnumerator;
+    }
+
+    public RichTextVisitor CreateVisitor()
+    {
+        return new(this, new());
+    }
+
+    public RichTextVisitor CreateVisitor(VisitationContextData contextData)
+    {
+        return new(this, contextData);
+    }
+}
+
+public readonly record struct Key(string Value);
+public readonly record struct Key<T>(Key Value);
+
+public enum VisitationAction
+{
+    Recurse,
+    Default = Recurse,
+    Stop,
+}
+
+public sealed class VisitationContextData()
+{
+    private struct V
+    {
+        public V(Key key) => Key = key;
+        public readonly Key Key;
+        public object? Value;
+    }
+    private readonly List<V> _values = new();
+    public VisitationAction Action { get; set; }
+
+    public void Clear() => _values.Clear();
+
+    public void Set<T>(Key<T> key, T value)
+    {
+        ref var x = ref GetOrAdd(key.Value);
+        x = value;
+    }
+    public T GetOrAdd<T>(Key<T> key, Func<T> factory)
+    {
+        ref object? obj = ref GetRef(key.Value, out bool exists);
+        if (exists)
+        {
+            return (T) obj!;
+        }
+        var ret = factory();
+        AddRef(key.Value) = ret;
+        return ret;
+    }
+    public void Add<T>(Key<T> key, T value)
+    {
+        _ = ref GetRef(key.Value, out bool exists);
+        if (exists)
+        {
+            throw new InvalidOperationException($"Cannot reset key '{key.Value.Value}' that has not been added!");
+        }
+        AddRef(key.Value) = value;
+    }
+    public void Reset<T>(Key<T> key, T value)
+    {
+        ref var x = ref GetRef(key.Value, out bool exists);
+        if (!exists)
+        {
+            throw new InvalidOperationException($"Cannot reset key '{key.Value.Value}' that has not been added!");
+        }
+        x = value;
+    }
+    public T Get<T>(Key<T> key)
+    {
+        ref object? obj = ref GetRef(key.Value, out bool exists);
+        if (!exists)
+        {
+            throw new InvalidOperationException($"Cannot reset key '{key.Value.Value}' that has not been added!");
+        }
+        return (T) obj!;
+    }
+    public bool TryGet<T>(Key<T> key, [NotNullWhen(true)] out T? value)
+    {
+        ref object? obj = ref GetRef(key.Value, out bool exists);
+        if (!exists)
+        {
+            value = default;
+        }
+        else
+        {
+            value = (T) obj!;
+        }
+        return exists;
+    }
+
+    private ref object? GetOrAdd(Key key)
+    {
+        {
+            ref object? obj = ref GetRef(key, out bool exists);
+            if (exists)
+            {
+                return ref obj;
+            }
+        }
+        {
+            return ref AddRef(key);
+        }
+    }
+
+    private ref object? AddRef(Key key)
+    {
+        _values.Add(new V(key));
+        return ref CollectionsMarshal.AsSpan(_values)[^1].Value;
+    }
+
+    private ref object? GetRef(Key key, out bool exists)
+    {
+        var span = CollectionsMarshal.AsSpan(_values);
+        for (int i = 0; i < span.Length; i++)
+        {
+            if (span[i].Key == key)
+            {
+                exists = true;
+                return ref span[i].Value;
+            }
+        }
+        exists = false;
+        return ref Unsafe.NullRef<object?>();
+    }
+}
+
+public readonly struct RichTextVisitor
+{
+    private readonly RichTextVisitationMap _visitationMap;
+    public VisitationContextData Data { get; }
+
+    public RichTextVisitor(
+        RichTextVisitationMap visitationMap,
+        VisitationContextData data)
+    {
+        _visitationMap = visitationMap;
+        Data = data;
+    }
+
+    public void Visit(IRichTextNode node)
+    {
+        var type = node.GetType();
+        var handler = _visitationMap.GetHandler(type);
+        handler(node, this);
+    }
+
+    public IEnumerator<IRichTextNode> GetChildren(IRichTextNode node)
+    {
+        return _visitationMap.GetChildren(node);
+    }
+}
+
+public static class RichTextVisitorDefaults
+{
+    private static RichTextVisitationMapBuilder Builder { get; } =
+        new RichTextVisitationMapBuilder()
+            .Children<Href>(node => SingleItemEnumerator.Create(node.Text))
+            .Children<RichText>(GetChildren)
+        // .OnUnhandled((node, c) =>
+        // {
+        //     _ = node;
+        //     _ = c;
+        // })
+        ;
+
+    private static IEnumerator<IRichTextNode> GetChildren(RichText rt)
+    {
+        foreach (var x in rt.Items)
+        {
+            yield return x;
+        }
+    }
+
+    public static RichTextVisitationMapBuilder CreateBuilder() => Builder.Copy();
 }
--- src/Tests/Core/Tests.cs
+++ src/Tests/Core/Tests.cs
@@ -1,3 +1,6 @@
+using System.Diagnostics;
+using System.Diagnostics.SymbolStore;
+
 namespace FindJobHelper.Core.Tests;
 
 public sealed class SomeTests
@@ -61,4 +64,212 @@ public sealed class SomeTests
         var ev = db.Experiences.Where(x => x.IsJob).SelectEvents(searchParams);
         await Verify(ev);
     }
+
+    [Fact]
+    public void RichTextTest()
+    {
+        var text = $"""
+        {RichTextFactory.Bold("Hello")} world,
+        {RichTextFactory.Href("https://Test.com", $"{RichTextFactory.Bold("url")}")}
+        Regular text
+        """;
+        const string expected = """
+        Hello world,
+        url
+        Regular text
+        """;
+        Assert.Equal(expected, text);
+    }
+
+    [Fact]
+    public void RichTextTestTreeEqual()
+    {
+        var text = RichText.Create($"""
+        {RichTextFactory.Bold("Hello")} world,
+        {RichTextFactory.Href("https://Test.com", RichTextFactory.Bold("url"))}
+        Regular text
+        """);
+        var expected = new RichText
+        {
+            Items = [
+                new StyledText
+                {
+                    Style = StyleFlags.Bold,
+                    Text = "Hello",
+                },
+                new PlainText
+                {
+                    Text = " world,\r\n",
+                },
+                new Href
+                {
+                    Text = new StyledText
+                    {
+                        Style = StyleFlags.Bold,
+                        Text = "url",
+                    },
+                    Url = new("https://Test.com"),
+                },
+                new PlainText
+                {
+                    Text = "\r\nRegular text",
+                },
+            ],
+        };
+        var ret = EqualityCheckVisitor.Compare(text, expected) ?? [];
+        Assert.Empty(ret);
+    }
+}
+
+public static class EqualityCheckVisitor
+{
+    public static List<string>? Compare(
+        RichText a,
+        RichText b)
+    {
+        var data = new VisitationContextData();
+        data.Add(StackKey, new()
+        {
+            Root = b,
+        });
+        var visitor = VisitationMap.CreateVisitor(data);
+        visitor.Visit(a);
+        data.TryGet(Errors, out var errors);
+        return errors;
+    }
+
+    private static void AddError(RichTextVisitor c, string error)
+    {
+        var errors = c.Data.GetOrAdd(Errors, () => new());
+        errors.Add(error);
+    }
+    private static void ReportChildCountMismatch(
+        IRichTextNode node,
+        RichTextVisitor c,
+        StackFrame frame)
+    {
+        c.Data.Action = VisitationAction.Stop;
+        AddError(c, $"Node '{node}' doesn't have the name number of children as '{frame.Node}'");
+    }
+
+    public static readonly RichTextVisitationMap VisitationMap = RichTextVisitorDefaults.CreateBuilder()
+        .Each(next => (node, c) =>
+        {
+            Before();
+            if (c.Data.Action != VisitationAction.Recurse)
+            {
+                return;
+            }
+            int countBefore = c.Data.Get(StackKey).Count;
+            next(node, c);
+
+            if (c.Data.Action != VisitationAction.Recurse)
+            {
+                return;
+            }
+            int countAfter = c.Data.Get(StackKey).Count;
+            Debug.Assert(countBefore == countAfter, "Popped extra frames?");
+            After();
+
+            void ReportChildCountMismatch1(StackFrame frame)
+            {
+                ReportChildCountMismatch(node, c, frame);
+            }
+
+            void Before()
+            {
+                var stack = c.Data.Get(StackKey);
+                IRichTextNode currentNode;
+                if (stack.CurrentFrame is { } frame)
+                {
+                    if (!frame.Children.MoveNext())
+                    {
+                        ReportChildCountMismatch1(frame);
+                        return;
+                    }
+                    currentNode = frame.Children.Current;
+                }
+                else
+                {
+                    currentNode = stack.Root;
+                }
+                stack.Add(new()
+                {
+                    Node = currentNode,
+                    Children = c.GetChildren(currentNode),
+                });
+            }
+            void After()
+            {
+                var stack = c.Data.Get(StackKey);
+                var frame = stack.CurrentFrame!;
+                if (frame.Children.MoveNext())
+                {
+                    ReportChildCountMismatch1(frame);
+                    return;
+                }
+
+                stack.Pop();
+            }
+        })
+        .Compare<Href>((a, b) => a.Url.Equals(b.Url))
+        .Compare<PlainText>((a, b) => a.Text.Equals(b.Text))
+        .Compare<StyledText>((a, b) => a.Text.Equals(b.Text) && a.Style.Equals(b.Style))
+        .Build();
+
+
+    extension(RichTextVisitationMapBuilder builder)
+    {
+        private RichTextVisitationMapBuilder Compare<T>(Func<T, T, bool> compareValue)
+            where T : IRichTextNode
+        {
+            return builder.Override<T>(next => (node, c) =>
+            {
+                var frame = c.Data.Get(StackKey).CurrentFrame!;
+                if (frame.Node is not T currentNode)
+                {
+                    AddError(c, $"Node types not equal: '{frame.Node.GetType()}' and '{typeof(T)}' with values '{frame.Node}' and '{node}'");
+                }
+                else if (!compareValue(currentNode, node))
+                {
+                    AddError(c, $"'{currentNode}' and '{node}' value not equal");
+                }
+
+                next(node, c);
+            });
+        }
+    }
+
+    public sealed class StackFrame
+    {
+        public required IRichTextNode Node;
+        public required IEnumerator<IRichTextNode> Children;
+    }
+    public sealed class Stack : List<StackFrame>
+    {
+        public required IRichTextNode Root { get; init; }
+        public StackFrame? CurrentFrame => Count == 0 ? null : this[Count - 1];
+        public IRichTextNode CurrentNode
+        {
+            get
+            {
+                if (Count == 0)
+                {
+                    return Root;
+                }
+                return CurrentFrame!.Children.Current;
+            }
+        }
+
+        public void Pop()
+        {
+            RemoveAt(Count - 1);
+        }
+    }
+    public sealed class ErrorContext : List<string>
+    {
+    }
+    public static readonly Key<ErrorContext> Errors = new(new("Errors"));
+
+    public static readonly Key<Stack> StackKey = new(new("Stack"));
 }
